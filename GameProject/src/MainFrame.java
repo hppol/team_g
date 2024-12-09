e78381cc85d5dedc022595d4083b8d08cca0cdf3
djdj9 <djdj9@LAPTOP-RQI4F73A>
2024-12-09 15:07:29 +0900

    123

--- GameProject/src/MainFrame.java
+++ GameProject/src/MainFrame.java
@@ -2,11 +2,11 @@ import javax.swing.JFrame;
 
 public class MainFrame extends JFrame {
 	public MainFrame() {
-		setTitle("Game");
-		setSize(800, 600);
-		setLocationRelativeTo(null);
-		add(new Screen());
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
+//		setTitle("Game");
+//		setSize(800, 600);
+//		setLocationRelativeTo(null);
+//		add(new Screen());
+//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		setVisible(true);
 	}
 }
